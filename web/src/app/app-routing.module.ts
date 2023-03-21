1cbd8ea7a709fb6743f7e300d2cf7d7d4f23c4a5
MariusTusvik <115139566+MariusTusvik@users.noreply.github.com>
2023-03-21 12:45:27 +0100

    basic setup

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontPageComponent } from './front-page/front-page.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', component: FrontPageComponent},  
+];
 
 @NgModule({
+  declarations: [
+   ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    NavbarComponent,
+    FooterComponent,
+
   ],
   imports: [
     BrowserModule,
