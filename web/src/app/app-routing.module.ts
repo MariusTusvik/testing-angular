a5253a7d468b5015fea4548e76c4f3a51348a75a
MariusTusvik <115139566+MariusTusvik@users.noreply.github.com>
2023-03-30 17:42:38 +0200

    routing

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { VideoComponent } from './video/video.component';
+import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   { path: '', component: FrontPageComponent },
   { path: 'video', component: VideoComponent },
   { path: 'tabs', component: TabsComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'categories', component: CategoriesComponent },
 ];
 
 @NgModule({
